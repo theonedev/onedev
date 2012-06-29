f56a52fab4c2e92db36560984239b6f586008286
robin <robin@pmease.com>
2012-06-29 21:39:09 +0800

    discard tapestry

--- commons.tapestry/src/main/java/com/pmease/commons/tapestry/extensionpoints/TapestryConfiguratorProvider.java
+++
@@ -1,5 +0,0 @@
-package com.pmease.commons.tapestry.extensionpoints;
-
-public interface TapestryConfiguratorProvider {
-	Class<?> getTapestryConfigurator();
-}
--- commons.tapestry/src/main/java/com/pmease/commons/tapestry/persistence/PersistenceConstants.java
+++
@@ -1,5 +0,0 @@
-package com.pmease.commons.tapestry.persistence;
-
-public class PersistenceConstants {
-	public static final String ENTITY = "entity";
-}
