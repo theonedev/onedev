03926fa1c2a0a3183f4c56ea276b7ad2a41e7242
robin shine <robin@pmease.com>
2013-06-30 20:47:18 +0800

    Test update parent pom version. Verify consistency of parent pom versions across projects in workspace.

--- plugin.maven/src/main/java/com/pmease/plugin/maven/CheckProjectDependenciesMojo.java
+++ plugin.maven/src/main/java/com/pmease/plugin/maven/CheckProjectDependenciesMojo.java
@@ -1,6 +1,7 @@
 package com.pmease.plugin.maven;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -9,12 +10,20 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.StringUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * @goal check-project-dependencies
@@ -48,6 +57,71 @@ public class CheckProjectDependenciesMojo extends AbstractMojo {
 				if (propsFile.exists()) {
 					Properties props = PluginUtils.loadProperties(propsFile);
 					projectVersions.put(props.getProperty("id"), props.getProperty("version"));
+				} else {
+					File pomFile = new File(projectDir, "pom.xml");
+					if (pomFile.exists()) {
+						try {
+							DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+							Document document = builder.parse(pomFile);
+							NodeList nodeList = document.getElementsByTagName("groupId");
+							String groupId = null;
+							if (nodeList.getLength() != 0) {
+								groupId = nodeList.item(0).getTextContent().trim();
+							} 
+							
+							nodeList = document.getElementsByTagName("artifactId");
+							if (nodeList.getLength() == 0) {
+								throw new MojoFailureException("Can not determine artifactId in POM '" + 
+										pomFile.getAbsolutePath() + "'.");
+							}
+							String artifactId = nodeList.item(0).getTextContent().trim();
+							
+							nodeList = document.getElementsByTagName("version");
+							String version = null;
+							if (nodeList.getLength() != 0)
+								version = nodeList.item(0).getTextContent().trim();
+							
+							if (groupId == null) {
+								nodeList = document.getElementsByTagName("parent");
+								if (nodeList.getLength() == 0) {
+									throw new MojoFailureException("Unable to determine groupId in POM '" + 
+											pomFile.getAbsolutePath() + "'.");
+								}
+								Element parentElement = (Element) nodeList.item(0);
+								
+								nodeList = parentElement.getElementsByTagName("groupId");
+								if (nodeList.getLength() == 0) {
+									throw new MojoFailureException("Unable to determine groupId in POM '" + 
+											pomFile.getAbsolutePath() + "'.");
+								}
+								groupId = nodeList.item(0).getTextContent().trim();
+							}
+
+							if (version == null) {
+								nodeList = document.getElementsByTagName("parent");
+								if (nodeList.getLength() == 0) {
+									throw new MojoFailureException("Unable to determine version in POM '" + 
+											pomFile.getAbsolutePath() + "'.");
+								}
+								Element parentElement = (Element) nodeList.item(0);
+								
+								nodeList = parentElement.getElementsByTagName("version");
+								if (nodeList.getLength() == 0) {
+									throw new MojoFailureException("Unable to determine version in POM '" + 
+											pomFile.getAbsolutePath() + "'.");
+								}
+								version = nodeList.item(0).getTextContent().trim();
+							}
+							
+							projectVersions.put(groupId + "." + artifactId, version);
+						} catch (ParserConfigurationException e) {
+							throw new MojoFailureException("Error parsing POM file '" + pomFile.getAbsolutePath() + "'.", e);
+						} catch (SAXException e) {
+							throw new MojoFailureException("Error parsing POM file '" + pomFile.getAbsolutePath() + "'.", e);
+						} catch (IOException e) {
+							throw new MojoFailureException("Error reading POM file '" + pomFile.getAbsolutePath() + "'.", e);
+						}
+					}
 				}
 			}
 		}
@@ -63,6 +137,18 @@ public class CheckProjectDependenciesMojo extends AbstractMojo {
 						errors.add("Dependency to project '" + projectKey + "' should use version '" 
 								+ projectVersion + "'");
 					}
+				}
+			}
+		}
+		
+		MavenProject parentProject = project.getParent();
+		if (parentProject != null) {
+			String projectKey = parentProject.getGroupId() + "." + parentProject.getArtifactId();
+			String projectVersion = projectVersions.get(projectKey);
+			if (projectVersion != null) {
+				if (!projectVersion.equals(parentProject.getVersion())) {
+					errors.add("Parent project '" + projectKey + "' should use version '" 
+							+ projectVersion + "'");
 				}
 			}
 		}
