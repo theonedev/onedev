a8e2cecd7b46a859b346766932cb2452962e8b33
robin shine <robin@pmease.com>
2015-02-04 18:21:24 +0800

    JGit performance test.

--- gitplex.product/src/test/java/gitplex/product/Test.java
+++ gitplex.product/src/test/java/gitplex/product/Test.java
@@ -1,23 +1,66 @@
 package gitplex.product;
 
 import java.io.File;
+import java.io.IOException;
 
-import org.eclipse.jgit.lib.FileMode;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.filter.PathFilter;
 
 import com.pmease.commons.git.AbstractGitTest;
-import com.pmease.commons.git.BlobInfo;
-import com.pmease.commons.git.Git;
-import com.pmease.commons.git.TreeNode;
 
 public class Test extends AbstractGitTest {
 
 	@org.junit.Test
 	public void test() {
+		/*
 		Git git = new Git(new File("w:\\linux\\.git"));
-		for (TreeNode node: git.listTree("master~1000", "include/media")) {
-			if (node.getMode() == FileMode.TYPE_FILE) {
-				System.out.println(new String(git.readBlob(new BlobInfo("master~1000", node.getPath(), node.getMode()))));
+		long len = 0;
+		for (TreeNode node: git.listTree("master", "include/media/")) {
+			len += git.readBlob(new BlobInfo("master", node.getPath(), node.getMode())).length;
+		}
+		System.out.println(len);
+		*/
+		
+		try {
+			Git git = Git.open(new File("w:\\mozilla\\.git"));
+
+			RevWalk revWalk = new RevWalk(git.getRepository());
+		    RevCommit commit = revWalk.parseCommit(git.getRepository().resolve("master"));	        
+		    TreeWalk treeWalk = new TreeWalk(git.getRepository());
+		    treeWalk.addTree(commit.getTree());
+	        treeWalk.setFilter(PathFilter.create("profile"));
+			treeWalk.setRecursive(true);
+			long len = 0;
+			while (treeWalk.next()) {
+				 ObjectId objectId = treeWalk.getObjectId(0);
+				 ObjectLoader loader = git.getRepository().open(objectId);
+				 len += loader.getBytes().length;
 			}
+			System.out.println(len);
+			
+			revWalk = new RevWalk(git.getRepository());
+		    commit = revWalk.parseCommit(git.getRepository().resolve("master~1000"));	        
+		    treeWalk = new TreeWalk(git.getRepository());
+		    treeWalk.addTree(commit.getTree());
+	        treeWalk.setFilter(PathFilter.create("profile"));
+			treeWalk.setRecursive(true);
+			len = 0;
+			while (treeWalk.next()) {
+				 ObjectId objectId = treeWalk.getObjectId(0);
+				 ObjectLoader loader = git.getRepository().open(objectId);
+				 len += loader.getBytes().length;
+			}
+			System.out.println(len);
+			
+			git.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
