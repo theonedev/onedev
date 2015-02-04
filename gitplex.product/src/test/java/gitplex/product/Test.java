5d330c571c2466a965173486c3c24f673f2af01e
robin shine <robin@pmease.com>
2015-02-04 17:29:36 +0800

    Resolve Javassist conflicts.

--- gitplex.product/src/test/java/gitplex/product/Test.java
+++ gitplex.product/src/test/java/gitplex/product/Test.java
@@ -1,10 +1,24 @@
 package gitplex.product;
 
-public class Test {
+import java.io.File;
+
+import org.eclipse.jgit.lib.FileMode;
+
+import com.pmease.commons.git.AbstractGitTest;
+import com.pmease.commons.git.BlobInfo;
+import com.pmease.commons.git.Git;
+import com.pmease.commons.git.TreeNode;
+
+public class Test extends AbstractGitTest {
 
 	@org.junit.Test
 	public void test() {
-		System.out.println("hello".contains(""));
+		Git git = new Git(new File("w:\\linux\\.git"));
+		for (TreeNode node: git.listTree("master~1000", "include/media")) {
+			if (node.getMode() == FileMode.TYPE_FILE) {
+				System.out.println(new String(git.readBlob(new BlobInfo("master~1000", node.getPath(), node.getMode()))));
+			}
+		}
 	}
 	
 }
