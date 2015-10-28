efce9ec098abbee1490fd10f033ce15633ca73a8
robin shine <robin@pmease.com>
2015-10-28 21:12:46 +0800

    midwork

--- gitplex.web/src/main/java/com/pmease/gitplex/web/page/repository/commit/CommitFilter.java
+++ gitplex.web/src/main/java/com/pmease/gitplex/web/page/repository/commit/CommitFilter.java
@@ -2,6 +2,20 @@ package com.pmease.gitplex.web.page.repository.commit;
 
 import java.io.Serializable;
 
-public interface CommitFilter extends Serializable {
+import javax.annotation.Nullable;
 
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import com.pmease.commons.git.command.LogCommand;
+
+public interface CommitFilter extends Serializable {
+	String getName();
+	
+	@Nullable
+	FilterEditor<?> newEditor(String id);
+	
+	void applyTo(LogCommand logCommand);
+	
+	void applyTo(PageParameters params);
+	
 }
