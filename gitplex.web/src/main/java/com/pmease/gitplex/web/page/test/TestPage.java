d6821c956af9f726fc628406c96294c6e813e8f3
robin shine <robin@pmease.com>
2015-10-31 19:09:11 +0800

    midwork

--- gitplex.web/src/main/java/com/pmease/gitplex/web/page/test/TestPage.java
+++ gitplex.web/src/main/java/com/pmease/gitplex/web/page/test/TestPage.java
@@ -1,6 +1,7 @@
 package com.pmease.gitplex.web.page.test;
 
 import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.html.link.Link;
 
 import com.pmease.gitplex.web.page.base.BasePage;
 
@@ -10,6 +11,14 @@ public class TestPage extends BasePage {
 	@Override
 	protected void onInitialize() {
 		super.onInitialize();
+		
+		add(new Link<Void>("test") {
+
+			@Override
+			public void onClick() {
+			}
+			
+		});
 	}
 
 	@Override
