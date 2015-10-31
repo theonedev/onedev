c3527b495a7e3c966411e7334f8b26850cea615f
robin shine <robin@pmease.com>
2015-10-31 21:03:36 +0800

    midwork

--- gitplex.web/src/main/java/com/pmease/gitplex/web/page/test/TestPage.java
+++ gitplex.web/src/main/java/com/pmease/gitplex/web/page/test/TestPage.java
@@ -2,12 +2,16 @@ package com.pmease.gitplex.web.page.test;
 
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.link.Link;
+import org.h2.mvstore.MVMap;
+import org.h2.mvstore.MVStore;
 
 import com.pmease.gitplex.web.page.base.BasePage;
 
 @SuppressWarnings("serial")
 public class TestPage extends BasePage {
 
+	private int count = 0;
+	
 	@Override
 	protected void onInitialize() {
 		super.onInitialize();
@@ -16,6 +20,18 @@ public class TestPage extends BasePage {
 
 			@Override
 			public void onClick() {
+				long time = System.currentTimeMillis();
+				MVStore s = MVStore.open("w:\\temp\\db5");
+
+				// create/get the map named "data"
+				MVMap<Long, String> map = s.openMap("data");
+
+				for (int i=1000000; i<1001000; i++)
+					map.get(i);
+				
+				// close the store (this will persist changes)
+				s.close();
+				System.out.println(System.currentTimeMillis()-time);
 			}
 			
 		});
