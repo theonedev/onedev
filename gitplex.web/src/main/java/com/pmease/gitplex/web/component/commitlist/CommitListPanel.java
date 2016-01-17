776b9ed32b8d852c1e68b074aaef644dd8643886
robin shine <robin@pmease.com>
2016-01-17 18:54:48 +0800

    imdwork

--- gitplex.web/src/main/java/com/pmease/gitplex/web/component/commitlist/CommitListPanel.java
+++ gitplex.web/src/main/java/com/pmease/gitplex/web/component/commitlist/CommitListPanel.java
@@ -1,17 +1,20 @@
 package com.pmease.gitplex.web.component.commitlist;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.request.resource.CssResourceReference;
+import org.joda.time.DateTime;
 
 import com.pmease.commons.git.Commit;
 import com.pmease.gitplex.core.model.Repository;
@@ -55,37 +58,56 @@ public class CommitListPanel extends Panel {
 			protected List<Commit> load() {
 				List<Commit> commits = commitsModel.getObject();
 				if (commits.size() > Constants.MAX_DISPLAY_COMMITS)
-					return commits.subList(commits.size()-Constants.MAX_DISPLAY_COMMITS, commits.size());
-				else 
-					return commits;
+					commits = commits.subList(commits.size()-Constants.MAX_DISPLAY_COMMITS, commits.size());
+				return separateByDate(commits);
 			}
 			
 		}) {
 
 			@Override
-			protected void populateItem(final ListItem<Commit> item) {
+			protected void populateItem(ListItem<Commit> item) {
 				Commit commit = item.getModelObject();
 				
-				item.add(new ContributorAvatars("avatar", commit.getAuthor(), commit.getCommitter()));
-
-				item.add(new CommitMessagePanel("message", repoModel, new AbstractReadOnlyModel<Commit>() {
-
-					@Override
-					public Commit getObject() {
-						return item.getModelObject();
-					}
+				if (commit != null) {
+					Fragment fragment = new Fragment("commit", "commitFrag", CommitListPanel.this);
+					fragment.add(new ContributorAvatars("avatar", commit.getAuthor(), commit.getCommitter()));
+					fragment.add(new CommitMessagePanel("message", repoModel, item.getModel()));
+					fragment.add(new ContributionPanel("contribution", commit.getAuthor(), commit.getCommitter()));
+					fragment.add(new HashAndCodePanel("hashAndCode", repoModel, commit.getHash()));
+					fragment.add(AttributeAppender.append("class", "commit clearfix"));
 					
-				}));
-
-				item.add(new ContributionPanel("author", commit.getAuthor(), commit.getCommitter()));
-				
-				item.add(new HashAndCodePanel("hashAndCode", repoModel, commit.getHash()));
+					item.add(fragment);
+				} else {
+					Fragment fragment = new Fragment("commit", "dateFrag", CommitListPanel.this);
+					DateTime dateTime = new DateTime(getModelObject().get(item.getIndex()+1).getCommitter().getWhen());
+					fragment.add(new Label("date", Constants.DATE_FORMATTER.print(dateTime)));
+					if (item.getIndex() == 0)
+						fragment.add(AttributeAppender.append("class", "date first"));
+					else
+						fragment.add(AttributeAppender.append("class", "date"));
+					item.add(fragment);
+				}				
 			}
 			
 		});
 		
 	}
 
+	private List<Commit> separateByDate(List<Commit> commits) {
+		List<Commit> separated = new ArrayList<>();
+		DateTime groupTime = null;
+		for (Commit commit: commits) {
+			DateTime commitTime = new DateTime(commit.getCommitter().getWhen());
+			if (groupTime == null || commitTime.getYear() != groupTime.getYear() 
+					|| commitTime.getDayOfYear() != groupTime.getDayOfYear()) {
+				groupTime = commitTime;
+				separated.add(null);
+			} 
+			separated.add(commit);
+		}
+		return separated;
+	}
+	
 	@Override
 	public void renderHead(IHeaderResponse response) {
 		super.renderHead(response);
