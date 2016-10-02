9b4240b76627e60d5194e476d45a5e4e685d4e5c
robin shine <robin@pmease.com>
2016-10-02 21:36:48 +0800

    Add commit message transform setting helps

--- gitplex.core/src/main/java/com/pmease/gitplex/core/entity/support/CommitMessageTransformSetting.java
+++ gitplex.core/src/main/java/com/pmease/gitplex/core/entity/support/CommitMessageTransformSetting.java
@@ -15,7 +15,8 @@ public class CommitMessageTransformSetting implements Serializable {
 	
 	private String replaceWith;
 
-	@Editable(order=100)
+	@Editable(order=100, description="Specify <a href='https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html'>Java pattern</a> "
+			+ "to match in the commit message, for instance, you may match JIRA issue key with <tt>([A-Z]{2,}-\\d+)</tt>")
 	@NotEmpty
 	public String getSearchFor() {
 		return searchFor;
@@ -25,7 +26,10 @@ public class CommitMessageTransformSetting implements Serializable {
 		this.searchFor = searchFor;
 	}
 
-	@Editable(order=200)
+	@Editable(order=200, description="Specify replacement for above matches, for the example "
+			+ "issue key pattern above, you may use <tt>&lt;a href='http://track.example.com/browse/$1'&gt;$1&lt;/a&gt;</tt> to "
+			+ "replace issue keys with issue links. Here <tt>$n</tt> represents nth capturing group in <tt>search for</tt> "
+			+ "pattern")
 	@NotEmpty
 	public String getReplaceWith() {
 		return replaceWith;
