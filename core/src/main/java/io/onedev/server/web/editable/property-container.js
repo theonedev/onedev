a67de74b01d0067ff477db9f2afe5a4972227691
robin shen <robin@pmease.com>
2018-06-15 15:05:11 +0800

    Add for attribute to editor checkbox label

--- core/src/main/java/io/onedev/server/web/editable/property-container.js
+++ core/src/main/java/io/onedev/server/web/editable/property-container.js
@@ -4,11 +4,13 @@ onedev.server.propertyContainer = {
 		var $input = $container.find(".value>input[type=checkbox]");
 		$input.css("margin-right", "8px");
 		$input.css("vertical-align", "text-top");
-		$input.parent().prev("label.name").insertAfter($input);
+		var $label = $input.parent().prev("label.name");
+		$label.attr("for", $input.attr("id")).insertAfter($input);
 		
 		$input = $container.find(".value>div>input[type=checkbox]");
 		$input.css("margin-right", "8px");
 		$input.css("vertical-align", "text-top");
-		$input.parent().parent().prev("label.name").insertAfter($input);
+		$label = $input.parent().parent().prev("label.name");
+		$label.attr("for", $input.attr("id")).insertAfter($input);
 	}
 }
