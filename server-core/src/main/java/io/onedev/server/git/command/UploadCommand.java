584ea2122772129d43417d65643db6444eb39944
Robin Shen <robin@onedev.io>
2019-08-10 21:37:44 +0800

    Fix the issue that shallow clone/fetch does not work

--- server-core/src/main/java/io/onedev/server/git/command/UploadCommand.java
+++ server-core/src/main/java/io/onedev/server/git/command/UploadCommand.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import com.google.common.base.Preconditions;
 
 import io.onedev.commons.utils.command.Commandline;
+import io.onedev.commons.utils.command.ExecuteResult;
 import io.onedev.commons.utils.command.LineConsumer;
 
 public class UploadCommand extends GitCommand<Void> {
@@ -43,14 +45,24 @@ public class UploadCommand extends GitCommand<Void> {
 		Commandline cmd = cmd();
 		cmd.addArgs("upload-pack", "--stateless-rpc", ".");
 		
-		cmd.execute(output, new LineConsumer() {
+		AtomicBoolean toleratedErrors = new AtomicBoolean(false);
+		ExecuteResult result = cmd.execute(output, new LineConsumer() {
 
 			@Override
 			public void consume(String line) {
-				logger.error(line);
+				// This error may happen during a normal shallow fetch/clone 
+				if (line.contains("The remote end hung up unexpectedly")) {
+					toleratedErrors.set(true);
+					logger.debug(line);
+				} else {
+					logger.error(line);
+				}
 			}
 			
-		}, input).checkReturnCode();
+		}, input);
+		
+		if (result.getReturnCode() != 0 && !toleratedErrors.get())
+			throw result.buildException();
 		
 		return null;
 	}
