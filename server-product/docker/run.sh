24a529da1006fbce00afe3cd940bd25030919d71
Robin Shen <robin@onedev.io>
2019-08-29 22:47:04 +0800

    Fix minor script issue

--- server-product/docker/run.sh
+++ server-product/docker/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -v /opt/onedev:/opt/onedev -v /usr/bin/git:/usr/bin/git -p 6610:6610 1dev/server
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -v /opt/onedev:/opt/onedev -p 6610:6610 1dev/server
