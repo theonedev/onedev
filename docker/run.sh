acb629295a6726f32a0652f28db72af1901e5a9b
Robin Shen <robin@onedev.io>
2019-10-28 17:21:53 +0800

    versioning server docker image

--- docker/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -v /opt/onedev:/opt/onedev -p 6610:6610 1dev/server
---
+++ server-product/build/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh 
+buildVersion=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout -f ../../pom.xml)
+mvn clean package -f ../../pom.xml
+
+mkdir ../target/docker
+cp docker/* ../target/docker
+unzip ../target/onedev-${buildVersion}.zip -d ../target/docker
+mv ../target/docker/onedev-${buildVersion} ../target/docker/onedev
+cp ../target/sandbox/site/lib/mysql* ../target/sandbox/site/lib/postgresql* ../target/docker/onedev/site/lib
+docker build -t 1dev/server:${buildVersion} ../target/docker
+docker push 1dev/server:${buildVersion}
+
+cp -r k8s ../target/k8s
+find ../target/k8s -name "*.yaml" | xargs sed -i -e "s/\${buildVersion}/${buildVersion}/g"
