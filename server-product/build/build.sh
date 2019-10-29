c4fd345eb6a6a2054525d74955f5b42373577044
Robin Shen <robin@onedev.io>
2019-10-29 10:14:53 +0800

    fail fast in build.sh

--- server-product/build/build.sh
+++ server-product/build/build.sh
@@ -1,4 +1,5 @@
-#!/bin/sh 
+#!/bin/bash 
+set -e
 buildVersion=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout -f ../../pom.xml)
 mvn clean package -f ../../pom.xml
 
