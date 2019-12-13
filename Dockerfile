e11c51d9996040e06c513dd33a48f6bf905bb3d9
Robin Shen <robin@pmease.com>
2019-12-13 22:19:32 +0800

    Fix server build environment dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 # Build the docker image with necessary tools to build onedev server docker image
-FROM ubuntu:18.04
-# Install maven in a separate command as otherwise its dependency OpenJDK 11 will be installed
-RUN apt-get update && apt-get install -y git openjdk-8-jdk-headless && apt-get install -y maven
+FROM docker:19.03.5-git
+RUN apk add openjdk8 maven
--- server-product/build/build.sh
+++ server-product/build/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/sh
 set -e
 buildVersion=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout -f ../../pom.xml)
 mvn clean package -f ../../pom.xml
@@ -9,6 +9,7 @@ unzip ../target/onedev-${buildVersion}.zip -d ../target/docker
 mv ../target/docker/onedev-${buildVersion} ../target/docker/onedev
 cp ../target/sandbox/site/lib/mysql* ../target/sandbox/site/lib/postgresql* ../target/docker/onedev/site/lib
 docker build -t 1dev/server:${buildVersion} ../target/docker
+docker login -u robinshen -p $@
 docker push 1dev/server:${buildVersion}
 
 cp -r k8s ../target/k8s
