1ca47ebc674013b1e36b222e58bea963575bc8f7
Robin Shen <robin@pmease.com>
2019-12-14 17:46:37 +0800

    Move server build environment docker file to a separate project

--- server-product/build/build-server-build-environment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-version=1.0
-docker build -t 1dev/server-build-environment:$version -f docker/Dockerfile.server-build-environment .
-docker login -u robinshen -p $@
-docker push 1dev/server-build-environment:$version
