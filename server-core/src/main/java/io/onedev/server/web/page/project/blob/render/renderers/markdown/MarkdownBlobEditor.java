d30b7f31586c37a97e69a89330da6744dda4aa2a
Robin Shen <robin@onedev.io>
2020-02-02 16:39:23 +0800

    Able to mention user and reference builds/issues/pullrequests from markdown file (in addition to various comment)

--- server-core/src/main/java/io/onedev/server/web/page/project/blob/render/renderers/markdown/MarkdownBlobEditor.java
+++ server-core/src/main/java/io/onedev/server/web/page/project/blob/render/renderers/markdown/MarkdownBlobEditor.java
@@ -2,6 +2,9 @@ package io.onedev.server.web.page.project.blob.render.renderers.markdown;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import javax.annotation.Nullable;
 
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.head.IHeaderResponse;
@@ -9,11 +12,26 @@ import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.markup.html.form.FormComponentPanel;
 import org.apache.wicket.model.Model;
 
+import com.google.common.collect.Sets;
+
 import io.onedev.commons.utils.StringUtils;
 import io.onedev.server.OneDev;
+import io.onedev.server.entitymanager.BuildManager;
+import io.onedev.server.entitymanager.IssueManager;
+import io.onedev.server.entitymanager.PullRequestManager;
+import io.onedev.server.entitymanager.UserManager;
+import io.onedev.server.model.Build;
+import io.onedev.server.model.Issue;
+import io.onedev.server.model.Project;
+import io.onedev.server.model.PullRequest;
+import io.onedev.server.model.User;
 import io.onedev.server.util.ContentDetector;
 import io.onedev.server.util.markdown.MarkdownManager;
+import io.onedev.server.util.match.MatchScoreProvider;
+import io.onedev.server.util.match.MatchScoreUtils;
+import io.onedev.server.web.component.markdown.AtWhoReferenceSupport;
 import io.onedev.server.web.component.markdown.MarkdownEditor;
+import io.onedev.server.web.component.markdown.UserMentionSupport;
 import io.onedev.server.web.page.project.blob.render.BlobRenderContext;
 import io.onedev.server.web.page.project.blob.render.BlobRenderContext.Mode;
 
@@ -49,6 +67,61 @@ abstract class MarkdownBlobEditor extends FormComponentPanel<byte[]> {
 				MarkdownManager markdownManager = OneDev.getInstance(MarkdownManager.class);
 				String html = markdownManager.render(markdown);
 				return markdownManager.process(context.getProject(), html, context);
+			}
+
+			@Override
+			protected UserMentionSupport getUserMentionSupport() {
+				return new UserMentionSupport() {
+
+					@Override
+					public List<User> findUsers(String query, int count) {
+						List<User> mentionables = OneDev.getInstance(UserManager.class).queryAndSort(Sets.newHashSet());
+						List<User> filtered = MatchScoreUtils.filterAndSort(mentionables, new MatchScoreProvider<User>() {
+
+							@Override
+							public double getMatchScore(User object) {
+								return object.getMatchScore(query) 
+										* (mentionables.size() - mentionables.indexOf(object)) 
+										/ mentionables.size();
+							}
+							
+						});
+						
+						if (filtered.size() > count)
+							return filtered.subList(0, count);
+						else
+							return filtered;
+					}
+					
+				};
+			}
+			
+			@Override
+			protected final AtWhoReferenceSupport getReferenceSupport() {
+				return new AtWhoReferenceSupport() {
+
+					@Override
+					public List<PullRequest> findPullRequests(@Nullable Project project, String query, int count) {
+						if (project == null)
+							project = context.getProject();
+						return OneDev.getInstance(PullRequestManager.class).query(project, query, count);
+					}
+
+					@Override
+					public List<Issue> findIssues(@Nullable Project project, String query, int count) {
+						if (project == null)
+							project = context.getProject();
+						return OneDev.getInstance(IssueManager.class).query(project, query, count);
+					}
+
+					@Override
+					public List<Build> findBuilds(@Nullable Project project, String query, int count) {
+						if (project == null)
+							project = context.getProject();
+						return OneDev.getInstance(BuildManager.class).query(project, query, count);
+					}
+					
+				};
 			}
 
 		});
