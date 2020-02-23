f27405fedd2403361c925601e7c0bfa5585cedb1
Andrea Del Bene <adelbene@apache.org>
2020-02-23 22:13:36 +0100

    Added validation for the SSH key pair inserted during config phase.

--- server-core/src/main/java/io/onedev/server/model/support/administration/SshSettings.java
+++ server-core/src/main/java/io/onedev/server/model/support/administration/SshSettings.java
@@ -3,6 +3,7 @@ package io.onedev.server.model.support.administration;
 import java.io.Serializable;
 import javax.validation.ConstraintValidatorContext;
 import org.hibernate.validator.constraints.NotEmpty;
+import io.onedev.server.git.ssh.SshKeyUtils;
 import io.onedev.server.util.validation.Validatable;
 import io.onedev.server.util.validation.annotation.ClassValidating;
 import io.onedev.server.web.editable.annotation.Editable;
@@ -17,10 +18,11 @@ public class SshSettings implements Serializable, Validatable {
     private String serverSshUrl;
 
     private String privateKey;
-    
+
     private String publicKey;
-    
-    @Editable(name="Ssh URL", order=90, description="Specify the URL to use with Git to access repositories via SSH")
+
+    @Editable(name = "Ssh URL", order = 90,
+            description = "Specify the URL to use with Git to access repositories via SSH")
     @NotEmpty
     public String getServerSshUrl() {
         return serverSshUrl;
@@ -29,8 +31,9 @@ public class SshSettings implements Serializable, Validatable {
     public void setServerSshUrl(String serverSshUrl) {
         this.serverSshUrl = serverSshUrl;
     }
-    
-    @Editable(name="Server private Key",  order=90, description="Specify the private key (in PEM format) used by SSH server to establish connections")
+
+    @Editable(name = "Server private Key", order = 90,
+            description = "Specify the private key (in PEM format) used by SSH server to establish connections")
     @Multiline
     @NotEmpty
     public String getPrivateKey() {
@@ -41,7 +44,8 @@ public class SshSettings implements Serializable, Validatable {
         this.privateKey = privateKey;
     }
 
-    @Editable(name="Server public Key", order=90, description="Specify the public key (in PEM format) used by SSH server to establish connections")
+    @Editable(name = "Server public Key", order = 90,
+            description = "Specify the public key (in PEM format) used by SSH server to establish connections")
     @Multiline
     @NotEmpty
     public String getPublicKey() {
@@ -54,7 +58,18 @@ public class SshSettings implements Serializable, Validatable {
 
     @Override
     public boolean isValid(ConstraintValidatorContext context) {
-        //TODO build a keypair to validate the inserted keys
-        return true;
+        boolean hasErrors = false;
+        String propertyNode = "privateKey";
+        try {
+            SshKeyUtils.decodePrivateKey(privateKey);
+            propertyNode = "publicKey";
+            SshKeyUtils.decodePublicKey(publicKey);
+        } catch (Exception e) {
+            context.buildConstraintViolationWithTemplate("The provided key is not valid. Please check it and try again.")
+                    .addPropertyNode(propertyNode).addConstraintViolation()
+                    .disableDefaultConstraintViolation();
+            hasErrors = true;
+        }
+        return !hasErrors;
     }
 }
