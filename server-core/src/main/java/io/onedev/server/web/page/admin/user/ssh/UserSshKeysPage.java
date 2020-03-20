ca35b3916ee7c74be58044a10de40d225f626d90
Andrea Del Bene <adelbene@apache.org>
2020-03-20 12:12:18 +0100

    Added "add new key" functionality to UserSshKeysPage

--- server-core/src/main/java/io/onedev/server/web/page/admin/user/ssh/UserSshKeysPage.java
+++ server-core/src/main/java/io/onedev/server/web/page/admin/user/ssh/UserSshKeysPage.java
@@ -1,16 +1,19 @@
 package io.onedev.server.web.page.admin.user.ssh;
 
 import java.util.List;
-
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-
 import io.onedev.server.OneDev;
 import io.onedev.server.OneException;
 import io.onedev.server.entitymanager.SshKeyManager;
 import io.onedev.server.model.SshKey;
 import io.onedev.server.model.User;
+import io.onedev.server.web.component.modal.ModalLink;
+import io.onedev.server.web.component.modal.ModalPanel;
 import io.onedev.server.web.page.admin.user.UserPage;
+import io.onedev.server.web.page.my.sshkeys.InsertSshKeyPanel;
 import io.onedev.server.web.page.my.sshkeys.SshKeysListPanel;
 
 @SuppressWarnings("serial")
@@ -41,6 +44,20 @@ public class UserSshKeysPage extends UserPage {
         };
 
         SshKeysListPanel keyList = new SshKeysListPanel("keyList", detachableModel);
+        
+        add(new ModalLink("newKey") {
+            
+            @Override
+            protected Component newContent(String id, ModalPanel modal) {
+                return new InsertSshKeyPanel(id, modal, user) {
+
+                    @Override
+                    protected void onSave(AjaxRequestTarget target) {
+                        target.add(keyList);
+                    }};
+            }
+        });
+        
         add(keyList.setOutputMarkupId(true));
     }
 }
