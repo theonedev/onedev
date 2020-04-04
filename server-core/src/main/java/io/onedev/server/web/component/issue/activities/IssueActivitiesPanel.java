edea7f0274810bda94c8f957baacc5e8ee4bc35b
Robin Shen <robin@onedev.io>
2020-04-04 14:26:43 +0800

    Do not display issue description and pull request description change
    This is in order not to clutter the UI

--- server-core/src/main/java/io/onedev/server/web/component/issue/activities/IssueActivitiesPanel.java
+++ server-core/src/main/java/io/onedev/server/web/component/issue/activities/IssueActivitiesPanel.java
@@ -44,6 +44,7 @@ import io.onedev.server.model.IssueComment;
 import io.onedev.server.model.Project;
 import io.onedev.server.model.PullRequest;
 import io.onedev.server.model.User;
+import io.onedev.server.model.support.issue.changedata.IssueDescriptionChangeData;
 import io.onedev.server.model.support.issue.changedata.IssueReferencedFromCodeCommentData;
 import io.onedev.server.model.support.issue.changedata.IssueReferencedFromIssueData;
 import io.onedev.server.model.support.issue.changedata.IssueReferencedFromPullRequestData;
@@ -134,7 +135,7 @@ public abstract class IssueActivitiesPanel extends Panel {
 					CodeComment comment = OneDev.getInstance(CodeCommentManager.class).get(referencedFromCodeCommentData.getCommentId());
 					if (comment != null)
 						otherActivities.add(new IssueChangeActivity(change));
-				} else {
+				} else if (!(change.getData() instanceof IssueDescriptionChangeData)) {
 					otherActivities.add(new IssueChangeActivity(change));
 				}
 			}
