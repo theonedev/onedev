2e26e5b52ba3388d9196ec3657c2cc78f64e5343
Robin Shen <robin@onedev.io>
2020-04-19 10:03:07 +0800

    fix a minor styling issue

--- server-core/src/main/java/io/onedev/server/web/page/project/blob/render/renderers/markdown/MarkdownBlobCssResourceReference.java
+++ server-core/src/main/java/io/onedev/server/web/page/project/blob/render/renderers/markdown/MarkdownBlobCssResourceReference.java
@@ -2,12 +2,12 @@ package io.onedev.server.web.page.project.blob.render.renderers.markdown;
 
 import io.onedev.server.web.page.base.BaseDependentCssResourceReference;
 
-public class MarkdownBlobResourceReference extends BaseDependentCssResourceReference {
+public class MarkdownBlobCssResourceReference extends BaseDependentCssResourceReference {
 
 	private static final long serialVersionUID = 1L;
 
-	public MarkdownBlobResourceReference() {
-		super(MarkdownBlobResourceReference.class, "markdown-blob.css");
+	public MarkdownBlobCssResourceReference() {
+		super(MarkdownBlobCssResourceReference.class, "markdown-blob.css");
 	}
 
 }
