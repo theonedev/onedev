c5a727bd0790e651ef6e2e0075647d93159ac05b
Robin Shen <robin@onedev.io>
2020-11-17 07:59:48 +0000

    Edit build.sh

--- server-product/k8s/build.sh
+++ server-product/k8s/build.sh
@@ -10,10 +10,10 @@ rm -rf k8s-resources
 cp -r ../k8s k8s-resources
 rm k8s-resources/build.sh
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	find k8s-resources -name "*.yaml" | xargs sed -i -e "s/\${buildVersion}/${buildVersion}/g"
-else
+if [[ "$OSTYPE" == "darwin"* ]]; then
 	find k8s-resources -name "*.yaml" | xargs sed -i '' "s/\${buildVersion}/${buildVersion}/g"
+else
+	find k8s-resources -name "*.yaml" | xargs sed -i -e "s/\${buildVersion}/${buildVersion}/g"
 fi
 
 zip -r k8s-resources.zip k8s-resources
