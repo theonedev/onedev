9c607ba3f922718ccd008cea7e5032d38f60295d
Michael Weimann <mail@michael-weimann.eu>
2020-11-20 10:14:06 +0100

    add project and prefix to Referenceable

--- server-core/src/main/java/io/onedev/server/util/Referenceable.java
+++ server-core/src/main/java/io/onedev/server/util/Referenceable.java
@@ -1,7 +1,13 @@
 package io.onedev.server.util;
 
+import io.onedev.server.model.Project;
+
 public interface Referenceable {
 	
+	Project getProject();
+	
 	long getNumber();
+	
+	String getPrefix();
 	
 }
