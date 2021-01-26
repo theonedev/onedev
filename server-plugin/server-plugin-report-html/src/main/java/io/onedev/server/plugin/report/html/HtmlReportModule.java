0501fcf5782ad62e86fe3c39c0857bc0687ad313
Robin Shen <robin@onedev.io>
2021-01-26 21:11:06 +0800

    Remove test code

--- server-plugin/server-plugin-report-html/src/main/java/io/onedev/server/plugin/report/html/HtmlReportModule.java
+++ server-plugin/server-plugin-report-html/src/main/java/io/onedev/server/plugin/report/html/HtmlReportModule.java
@@ -8,19 +8,12 @@ import java.util.concurrent.Callable;
 
 import org.apache.wicket.protocol.http.WebApplication;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 import io.onedev.commons.launcher.loader.AbstractPluginModule;
 import io.onedev.commons.launcher.loader.ImplementationProvider;
 import io.onedev.commons.utils.LockUtils;
-import io.onedev.commons.utils.PlanarRange;
 import io.onedev.server.buildspec.job.JobReport;
-import io.onedev.server.code.CodeProblem;
-import io.onedev.server.code.CodeProblem.Severity;
-import io.onedev.server.code.CodeProblemContribution;
-import io.onedev.server.code.LineCoverage;
-import io.onedev.server.code.LineCoverageContribution;
 import io.onedev.server.model.Build;
 import io.onedev.server.security.SecurityUtils;
 import io.onedev.server.web.WebApplicationConfigurator;
@@ -89,26 +82,6 @@ public class HtmlReportModule extends AbstractPluginModule {
 			
 		});		
 
-		contribute(CodeProblemContribution.class, new CodeProblemContribution() {
-			
-			@Override
-			public List<CodeProblem> getCodeProblems(Build build, String blobPath, String reportName) {
-				return Lists.newArrayList(
-						new CodeProblem(new PlanarRange(21, 1, 21, 5), "message1", Severity.ERROR),
-						new CodeProblem(new PlanarRange(227, 1, 230, 10), "message4", Severity.WARNING)
-					);
-			}
-			
-		});
-		
-		contribute(LineCoverageContribution.class, new LineCoverageContribution() {
-			
-			@Override
-			public List<LineCoverage> getLineCoverages(Build build, String blobPath, String reportName) {
-				return Lists.newArrayList(new LineCoverage(21, 21, 1), new LineCoverage(226, 229, 0));
-			}
-			
-		});
 	}
 
 }
