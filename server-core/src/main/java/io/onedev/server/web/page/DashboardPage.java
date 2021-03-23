628d5c5e9c78e19e024ac68531a0ffb4096778ed
Robin Shen <robin@onedev.io>
2021-03-23 14:12:32 +0800

    Do not change url when redirect to project list page when access dashboard

--- server-core/src/main/java/io/onedev/server/web/page/DashboardPage.java
+++ server-core/src/main/java/io/onedev/server/web/page/DashboardPage.java
@@ -2,6 +2,8 @@ package io.onedev.server.web.page;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.RestartResponseException;
+import org.apache.wicket.core.request.handler.PageProvider;
+import org.apache.wicket.core.request.handler.RenderPageRequestHandler.RedirectPolicy;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
@@ -14,7 +16,8 @@ public class DashboardPage extends LayoutPage {
 	public DashboardPage(PageParameters params) {
 		super(params);
 		
-		throw new RestartResponseException(ProjectListPage.class, ProjectListPage.paramsOf(0, 0));
+		PageProvider pageProvider = new PageProvider(ProjectListPage.class, ProjectListPage.paramsOf(0, 0));
+		throw new RestartResponseException(pageProvider, RedirectPolicy.NEVER_REDIRECT);
 	}
 
 	@Override
