95d849e5f4901584434fd87bff4e159385567107
Robin Shen <robin@onedev.io>
2021-05-19 08:17:22 +0800

    fix issue #284 - Couldn't create new issue

--- server-core/src/main/java/io/onedev/server/security/permission/EditIssueField.java
+++ server-core/src/main/java/io/onedev/server/security/permission/EditIssueField.java
@@ -2,13 +2,15 @@ package io.onedev.server.security.permission;
 
 import java.util.Collection;
 
+import javax.annotation.Nullable;
+
 import org.apache.shiro.authz.Permission;
 
 public class EditIssueField implements Permission {
 
 	private final Collection<String> fields;
 	
-	public EditIssueField(Collection<String> fields) {
+	public EditIssueField(@Nullable Collection<String> fields) {
 		this.fields = fields;
 	}
 	
@@ -16,7 +18,12 @@ public class EditIssueField implements Permission {
 	public boolean implies(Permission p) {
 		if (p instanceof EditIssueField) {
 			EditIssueField editIssueField = (EditIssueField) p;
-			return fields.containsAll(editIssueField.fields);
+			if (fields == null)
+				return true;
+			else if (editIssueField.fields == null)
+				return false;
+			else
+				return fields.containsAll(editIssueField.fields);
 		} else {
 			return new AccessProject().implies(p);
 		}
