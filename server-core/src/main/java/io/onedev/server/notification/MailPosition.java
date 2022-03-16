56196afbce8d39c761d9d62eed3f791b62503933
Shen YuJiang <yjshen@hotmail.com>
2022-03-16 22:03:24 +0800

    Fix issue #615 - Issues not being created

---
+++ server-core/src/main/java/io/onedev/server/notification/MailPosition.java
@@ -0,0 +1,29 @@
+package io.onedev.server.notification;
+
+import java.io.Serializable;
+
+public class MailPosition implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private volatile long uidValidity = -1;
+	
+	private volatile long uid = 0;
+
+	public long getUidValidity() {
+		return uidValidity;
+	}
+
+	public void setUidValidity(long uidValidity) {
+		this.uidValidity = uidValidity;
+	}
+
+	public long getUid() {
+		return uid;
+	}
+
+	public void setUid(long uid) {
+		this.uid = uid;
+	}
+	
+}
