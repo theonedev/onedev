d3e0e172ac04c9a96598c03d9928e3fc6d819f63
Robin Shen <robin@onedev.io>
2022-05-25 13:23:03 +0800

    Fix issue #738 - Set JVM parameters for docker container

--- server-product/docker/entrypoint.sh
+++ server-product/docker/entrypoint.sh
@@ -11,14 +11,19 @@ _sigterm() {
 trap _sigterm SIGTERM
 trap _sigterm SIGINT
 
+JVM_OPTIONS=--XX:MaxRAMPercentage=50.0
+if [[ $JAVA_TOOL_OPTIONS == *"--XX:MaxRAMPercentage"* ]] || [[ $JAVA_TOOL_OPTIONS == *"-Xmx"* ]]; then
+  unset JVM_OPTIONS
+fi
+
 cd /app/bin
-java -cp "../boot/*" -XX:MaxRAMPercentage=50.0 io.onedev.commons.bootstrap.Bootstrap upgrade /opt/onedev &
+java -cp "../boot/*" ${JVM_OPTIONS} io.onedev.commons.bootstrap.Bootstrap upgrade /opt/onedev &
 child=$!
 wait "$child"
 
 touch /opt/onedev/IN_DOCKER
 
 cd /opt/onedev/bin
-java -cp "../boot/*" -XX:MaxRAMPercentage=50.0 io.onedev.commons.bootstrap.Bootstrap &
+java -cp "../boot/*" ${JVM_OPTIONS} io.onedev.commons.bootstrap.Bootstrap &
 child=$!
 wait "$child"
