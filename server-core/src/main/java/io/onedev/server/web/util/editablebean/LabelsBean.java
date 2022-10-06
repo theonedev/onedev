709fe2dec3f7c08950ea8bed111687e4a04b7c28
Robin Shen <robin@onedev.io>
2022-10-07 07:35:23 +0800

    Add label management help

--- server-core/src/main/java/io/onedev/server/web/util/editablebean/LabelsBean.java
+++ server-core/src/main/java/io/onedev/server/web/util/editablebean/LabelsBean.java
@@ -5,11 +5,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
 import io.onedev.server.OneDev;
 import io.onedev.server.entitymanager.LabelManager;
 import io.onedev.server.model.support.LabelSupport;
+import io.onedev.server.security.SecurityUtils;
 import io.onedev.server.web.editable.annotation.ChoiceProvider;
 import io.onedev.server.web.editable.annotation.Editable;
+import io.onedev.server.web.page.admin.labelmanagement.LabelManagementPage;
 
 @Editable
 public class LabelsBean implements Serializable {
@@ -18,7 +23,7 @@ public class LabelsBean implements Serializable {
 
 	private List<String> labels = new ArrayList<>();
 
-	@Editable
+	@Editable(descriptionProvider="getLabelsDescription")
 	@ChoiceProvider("getLabelChoices")
 	public List<String> getLabels() {
 		return labels;
@@ -32,6 +37,16 @@ public class LabelsBean implements Serializable {
 	private static List<String> getLabelChoices() {
 		var labels = OneDev.getInstance(LabelManager.class).query();
 		return labels.stream().map(it->it.getName()).sorted().collect(Collectors.toList());
+	}
+	
+	@SuppressWarnings("unused")
+	private static String getLabelsDescription() {
+		if (SecurityUtils.isAdministrator()) {
+			CharSequence url = RequestCycle.get().urlFor(LabelManagementPage.class, new PageParameters());
+			return String.format("Labels can be defined in <a href='%s' target='_blank'>Administration / Label Management</a>", url);
+		} else {
+			return "Labels can be defined in Administration / Label Management";
+		}
 	}
 
 	public static LabelsBean of(LabelSupport<?> labelSupport) {
