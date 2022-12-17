fba099cc452d4f04961d9726532694079553149e
Robin Shen <robin@onedev.io>
2022-12-17 16:48:33 +0800

    Tune some paths and maven phases to facilitate IntelliJ launch

--- server-core/src/main/java/io/onedev/server/web/page/test/TestPage.java
+++ server-core/src/main/java/io/onedev/server/web/page/test/TestPage.java
@@ -1,5 +1,6 @@
 package io.onedev.server.web.page.test;
 
+import io.onedev.commons.utils.FileUtils;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
