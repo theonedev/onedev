749e9c54d5f1cea32e62cbc71ad763622ed9b872
Andreas Wachter <buddyspencer@protonmail.com>
2023-01-25 16:01:14 +0000

    Pull request #55 - enable multi-arch builds
    to enable this change, please merge https://code.onedev.io/onedev/build-environment/~pulls/1 first, because `buildx` is needed.
    
    this would build 1dev/server and 1dev/agent as amd64 and arm64 and push them to hub.docker.com
    
    Fixes issue #797 - ARM64 support for 1dev/agent docker image
    Fixes issue #389 - OneDev docker image for arm/aarch64

--- server-product/docker/build.sh
+++ server-product/docker/build.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+function finish {
+  docker buildx stop builder
+  docker buildx rm builder
+}
+trap finish EXIT
+
 cd ../target
 rm -rf docker
 cp -r ../docker docker
@@ -11,7 +17,10 @@ unzip onedev-$buildVersion.zip -d docker
 mv docker/onedev-$buildVersion docker/app
 cp sandbox/site/lib/mysql* sandbox/site/lib/postgresql* docker/app/site/lib
 
-docker build -f docker/Dockerfile.server -t 1dev/server:$buildVersion docker
+docker buildx create --name builder
+docker buildx use builder
+
+docker buildx build --push --platform linux/amd64,linux/arm64 -f docker/Dockerfile.server -t 1dev/server:$buildVersion -t 1dev/server:latest docker
 
 cp -r agent docker/
-docker build -f docker/Dockerfile.agent -t 1dev/agent:latest docker
+docker buildx build --push --platform linux/amd64,linux/arm64 -f docker/Dockerfile.agent -t 1dev/agent:latest docker
