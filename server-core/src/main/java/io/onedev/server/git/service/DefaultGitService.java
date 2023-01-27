10335ae23036dd2372ac33ae8e3bc5f49a64ec61
Robin Shen <robin@onedev.io>
2023-01-27 22:12:24 +0800

    Fix a minor issue of not able to call git hooks when update pull request between two projects

--- server-core/src/main/java/io/onedev/server/git/service/DefaultGitService.java
+++ server-core/src/main/java/io/onedev/server/git/service/DefaultGitService.java
@@ -1,69 +1,9 @@
 package io.onedev.server.git.service;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.ObjectStreamException;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.stream.Collectors;
-
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-import org.apache.commons.lang3.SerializationUtils;
-import org.bouncycastle.openpgp.PGPSecretKeyRing;
-import org.eclipse.jgit.api.CreateBranchCommand;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.diff.DiffEntry;
-import org.eclipse.jgit.errors.MissingObjectException;
-import org.eclipse.jgit.errors.RevisionSyntaxException;
-import org.eclipse.jgit.lib.AnyObjectId;
-import org.eclipse.jgit.lib.CommitBuilder;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.FileMode;
-import org.eclipse.jgit.lib.ObjectDatabase;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.ObjectInserter;
-import org.eclipse.jgit.lib.PersonIdent;
-import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.RefUpdate;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.TagBuilder;
-import org.eclipse.jgit.lib.TreeFormatter;
-import org.eclipse.jgit.revwalk.LastCommitsOfChildren;
-import org.eclipse.jgit.revwalk.LastCommitsOfChildren.Value;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevTree;
-import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.jgit.revwalk.RevWalkUtils;
-import org.eclipse.jgit.revwalk.filter.RevFilter;
-import org.eclipse.jgit.treewalk.TreeWalk;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Sets;
-
 import io.onedev.commons.loader.ManagedSerializedForm;
-import io.onedev.commons.utils.ExceptionUtils;
-import io.onedev.commons.utils.ExplicitException;
-import io.onedev.commons.utils.FileUtils;
-import io.onedev.commons.utils.LinearRange;
-import io.onedev.commons.utils.LockUtils;
-import io.onedev.commons.utils.StringUtils;
+import io.onedev.commons.utils.*;
 import io.onedev.commons.utils.command.Commandline;
 import io.onedev.commons.utils.command.LineConsumer;
 import io.onedev.server.cluster.ClusterManager;
@@ -73,22 +13,8 @@ import io.onedev.server.entitymanager.SettingManager;
 import io.onedev.server.event.ListenerRegistry;
 import io.onedev.server.event.project.DefaultBranchChanged;
 import io.onedev.server.event.project.RefUpdated;
-import io.onedev.server.git.BlameBlock;
-import io.onedev.server.git.Blob;
-import io.onedev.server.git.BlobContent;
-import io.onedev.server.git.BlobEdits;
-import io.onedev.server.git.BlobIdent;
-import io.onedev.server.git.BlobIdentFilter;
-import io.onedev.server.git.CommandUtils;
-import io.onedev.server.git.GitTask;
-import io.onedev.server.git.GitUtils;
-import io.onedev.server.git.Submodule;
-import io.onedev.server.git.command.BlameCommand;
-import io.onedev.server.git.command.GetRawCommitCommand;
-import io.onedev.server.git.command.GetRawTagCommand;
-import io.onedev.server.git.command.ListChangedFilesCommand;
-import io.onedev.server.git.command.RevListOptions;
-import io.onedev.server.git.command.RevListCommand;
+import io.onedev.server.git.*;
+import io.onedev.server.git.command.*;
 import io.onedev.server.git.exception.NotFileException;
 import io.onedev.server.git.exception.NotTreeException;
 import io.onedev.server.git.exception.ObjectAlreadyExistsException;
@@ -97,6 +23,31 @@ import io.onedev.server.model.Project;
 import io.onedev.server.persistence.SessionManager;
 import io.onedev.server.persistence.annotation.Sessional;
 import io.onedev.server.storage.StorageManager;
+import org.apache.commons.lang3.SerializationUtils;
+import org.bouncycastle.openpgp.PGPSecretKeyRing;
+import org.eclipse.jgit.api.CreateBranchCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.errors.RevisionSyntaxException;
+import org.eclipse.jgit.lib.*;
+import org.eclipse.jgit.revwalk.*;
+import org.eclipse.jgit.revwalk.LastCommitsOfChildren.Value;
+import org.eclipse.jgit.revwalk.filter.RevFilter;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+import java.util.*;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.stream.Collectors;
 
 @Singleton
 public class DefaultGitService implements GitService, Serializable {
@@ -585,30 +536,25 @@ public class DefaultGitService implements GitService, Serializable {
 		runOnProjectServer(sourceProjectId, new ClusterTask<Void>() {
 
 			private static final long serialVersionUID = 1L;
-
-			private void push(Commandline git, String remoteUrl) {
-				git.workingDir(getGitDir(sourceProjectId));
-				git.addArgs("push", "--quiet", remoteUrl);
-				git.addArgs(refSpecs);
-				git.execute(newInfoLogger(), newErrorLogger()).checkReturnCode();
-			}
 			
 			@Override
 			public Void call() throws Exception {
 				UUID targetStorageServerUUID = projectManager.getStorageServerUUID(targetProjectId, true);
-				if (targetStorageServerUUID.equals(clusterManager.getLocalServerUUID())) {
-					push(CommandUtils.newGit(), getGitDir(targetProjectId).getAbsolutePath());
-				} else {
-					CommandUtils.callWithClusterCredential(new GitTask<Void>() {
+				
+				// Do not optimize to push to local directory when source and target are on same host, as otherwise
+				// environments in git pre/post receive hooks will not be set
+				CommandUtils.callWithClusterCredential(new GitTask<Void>() {
 
-						@Override
-						public Void call(Commandline git) {
-							push(git, clusterManager.getServerUrl(targetStorageServerUUID) + "/" + targetProjectPath);
-							return null;
-						}
-						
-					});
-				}
+					@Override
+					public Void call(Commandline git) {
+						git.workingDir(getGitDir(sourceProjectId));
+						git.addArgs("push", "--quiet", clusterManager.getServerUrl(targetStorageServerUUID) + "/" + targetProjectPath);
+						git.addArgs(refSpecs);
+						git.execute(newInfoLogger(), newErrorLogger()).checkReturnCode();
+						return null;
+					}
+					
+				});
 				return null;
 			}
 			
