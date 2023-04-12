9dfb37ed42e79423b2e88b48e4f2040a1881482d
Robin Shen <robin@onedev.io>
2023-04-12 08:52:22 +0800

    Fix issue #1291 - PullRequest / Commit UI: HTML renders escaped special characters instead of showing the escaped character

--- server-core/src/main/java/io/onedev/server/web/component/diff/blob/text/BlobTextDiffPanel.java
+++ server-core/src/main/java/io/onedev/server/web/component/diff/blob/text/BlobTextDiffPanel.java
@@ -360,10 +360,9 @@ public class BlobTextDiffPanel extends Panel {
 					StringBuilder builder = new StringBuilder();
 					appendEquals(builder, index, lastContextSize, contextSize);
 					
-					String expanded = StringUtils.replace(builder.toString(), "\"", "\\\"");
-					expanded = StringUtils.replace(expanded, "\n", "");
+					String expanded = StringUtils.replace(builder.toString(), "\n", "");
 					String script = String.format("onedev.server.blobTextDiff.expand('%s', %d, \"%s\");",
-							getMarkupId(), index, expanded);
+							getMarkupId(), index, JavaScriptEscape.escapeJavaScript(expanded));
 					target.appendJavaScript(script);
 					break;
 				case "openSelectionPopover":
