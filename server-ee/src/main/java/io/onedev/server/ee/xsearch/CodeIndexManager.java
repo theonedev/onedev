a3a5c92d01e7ff65123393f808307f23d2324663
Robin Shen <robin@onedev.io>
2023-05-25 12:56:45 +0800

    chore: remove ee module

--- server-ee/src/main/java/io/onedev/server/ee/xsearch/CodeIndexManager.java
+++
@@ -1,5 +0,0 @@
-package io.onedev.server.ee.xsearch;
-
-public interface CodeIndexManager {
-	
-}
--- server-ee/src/main/java/io/onedev/server/ee/xsearch/CodeSearchCssResourceReference.java
+++
@@ -1,9 +0,0 @@
-package io.onedev.server.ee.xsearch;
-
-import io.onedev.server.web.page.base.BaseDependentCssResourceReference;
-
-public class CodeSearchCssResourceReference extends BaseDependentCssResourceReference {
-	public CodeSearchCssResourceReference() {
-		super(CodeSearchCssResourceReference.class, "code-search.css");
-	}
-}
