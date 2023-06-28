7cbf8fcdf0efee679940eb042a1c1e5aa40e9116
Robin Shen <robin@onedev.io>
2023-06-28 10:22:15 +0800

    fix: user with issue management permission can not access confidential issues (#1442)

--- server-core/src/main/java/io/onedev/server/security/permission/ManageIssues.java
+++ server-core/src/main/java/io/onedev/server/security/permission/ManageIssues.java
@@ -7,6 +7,7 @@ public class ManageIssues implements Permission {
 	@Override
 	public boolean implies(Permission p) {
 		return p instanceof ManageIssues 
+				|| new AccessConfidentialIssues().implies(p)
 				|| new EditIssueField(null).implies(p) 
 				|| new EditIssueLink(null).implies(p) 
 				|| new ScheduleIssues().implies(p);
