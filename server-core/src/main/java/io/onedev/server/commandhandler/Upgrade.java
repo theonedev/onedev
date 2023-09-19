8c384074a12df3f685a495985cbd6a06b4bf2c17
Robin Shen <robin@onedev.io>
2023-09-19 20:19:01 +0800

    feat: improve upgrade procedure to make it more robust upon upgrade failure (#1569)

--- server-core/src/main/java/io/onedev/server/commandhandler/Upgrade.java
+++ server-core/src/main/java/io/onedev/server/commandhandler/Upgrade.java
@@ -233,6 +233,16 @@ public class Upgrade extends AbstractPlugin {
 			Properties oldReleaseProps = loadReleaseProps(new File(upgradeDir, RELEASE_PROPS_FILE));
 
 			if (!releaseProps.equals(oldReleaseProps)) {
+				var upgradingFile = new File(upgradeDir, "upgrading");
+				if (upgradingFile.exists()) {
+					logger.error("Last upgrade not completed yet. If you believe it already done, " +
+							"please remove file '" + upgradingFile.getAbsolutePath() + "' and try " +
+							"upgrade again");
+					System.exit(1);
+				}
+
+				FileUtils.touchFile(upgradingFile);
+
 				logger.info("Upgrading {}...", upgradeDir.getAbsolutePath());
 
 				if (OneDev.isServerRunning(upgradeDir)) {
@@ -478,19 +488,27 @@ public class Upgrade extends AbstractPlugin {
 										logger.error("Error restoring old database", e);
 									}
 								}
+								
+								if (programRestored && !dbChanged)
+									FileUtils.deleteFile(upgradingFile);
 
 								StringBuilder errorMessage = new StringBuilder(String.format("!! Failed to upgrade %s", upgradeDir.getAbsolutePath()));
 								if (dbChanged) {
-									errorMessage.append("\nOneDev is unable to restore old database, please do it manually by first resetting it (delete and create), and then running below command:");
+									if (new File(upgradeDir, "IN_DOCKER").exists())
+										errorMessage.append("\nOneDev is unable to restore old database, please do it manually by first resetting it (delete and create), and then running below command *INSIDE* the container:");
+									else
+										errorMessage.append("\nOneDev is unable to restore old database, please do it manually by first resetting it (delete and create), and then running below command:");										
 									if (SystemUtils.IS_OS_WINDOWS) {
-										errorMessage.append("\n" + upgradeDir.getAbsolutePath() + File.separator + "bin" + File.separator + "restore-db.bat " + dbBackupFile.getAbsolutePath() + " false");
+										errorMessage.append("\n\n" + upgradeDir.getAbsolutePath() + File.separator + "bin" + File.separator + "restore-db.bat " + dbBackupFile.getAbsolutePath() + " false");
 									} else {
-										errorMessage.append("\n" + upgradeDir.getAbsolutePath() + File.separator + "bin" + File.separator + "restore-db.sh " + dbBackupFile.getAbsolutePath() + " false");
+										errorMessage.append("\n\n" + upgradeDir.getAbsolutePath() + File.separator + "bin" + File.separator + "restore-db.sh " + dbBackupFile.getAbsolutePath() + " false");
 									}
+									errorMessage.append("\n\n*** Do not forget to remove file '" + upgradingFile.getAbsolutePath() + "' after problem is solved ***");
 								}
 								throw new ExplicitException(errorMessage.toString());
 							} else {
 								logger.info("Successfully upgraded {}", upgradeDir.getAbsolutePath());
+								FileUtils.deleteFile(upgradingFile);
 								FileUtils.deleteDir(programBackup);
 							}
 							return null;
@@ -498,7 +516,7 @@ public class Upgrade extends AbstractPlugin {
 					};
 					
 					var maintenanceFile = OneDev.getMaintenanceFile(upgradeDir);
-					maintenanceFile.createNewFile();
+					FileUtils.touchFile(maintenanceFile);
 					try {
 						var hibernateConfig = new HibernateConfig(upgradeDir);
 						if (!hibernateConfig.isHSQLDialect()) {
@@ -510,7 +528,7 @@ public class Upgrade extends AbstractPlugin {
 							callable.call();
 						}
 					} finally {
-						maintenanceFile.delete();
+						FileUtils.deleteFile(maintenanceFile);
 					}
 					System.exit(0);
 				} catch (ExplicitException e) {
