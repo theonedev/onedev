ce82c79d124b27e444c44da41551a1b459a12930
Robin Shen <robin@onedev.io>
2023-10-18 19:39:46 +0800

    chore: minor help fix

--- server-core/src/main/java/io/onedev/server/model/support/administration/BackupSetting.java
+++ server-core/src/main/java/io/onedev/server/model/support/administration/BackupSetting.java
@@ -25,7 +25,7 @@ public class BackupSetting implements Serializable, Validatable {
 			"For example, <em>0 0 1 * * ?</em> means 1:00am every day. For details of the format, refer " +
 			"to <a href='http://www.quartz-scheduler.org/documentation/quartz-2.3.0/tutorials/crontrigger.html#format' target='_blank'>Quartz tutorial</a>." +
 			"The backup files will be placed into <em>db-backup</em> folder under OneDev " +
-			"installation directory. In case multiple servers connect to form a cluster, auto-backup takes place on the <a href='https://docs.onedev.io/concepts#lead-server'>lead server</a>. " +
+			"installation directory. In case multiple servers connect to form a cluster, auto-backup takes place on the <a href='https://docs.onedev.io/concepts#lead-server' target='_blank'>lead server</a>. " +
 			"Leave this property empty if you do not want to enable database auto backup.")
 	@NotEmpty
 	public String getSchedule() {
