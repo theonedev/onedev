293d67a0930b790fbb0d79e0951b6ca12b1a02e0
Robin Shen <robin@onedev.io>
2023-12-02 14:18:26 +0800

    fix: Package management option of new project does not work (#1673)

--- server-core/src/main/java/io/onedev/server/web/page/project/NewProjectPage.java
+++ server-core/src/main/java/io/onedev/server/web/page/project/NewProjectPage.java
@@ -82,6 +82,7 @@ public class NewProjectPage extends LayoutPage {
 						newProject.setDescription(editProject.getDescription());
 						newProject.setCodeManagement(editProject.isCodeManagement());
 						newProject.setIssueManagement(editProject.isIssueManagement());
+						newProject.setPackManagement(editProject.isPackManagement());
 						newProject.setTimeTracking(editProject.isTimeTracking());
 						newProject.setDefaultRole(defaultRoleBean.getRole());
 						
