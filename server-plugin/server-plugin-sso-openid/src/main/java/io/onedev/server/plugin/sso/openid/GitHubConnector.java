b2fecfc83a0b8a07ae6950896b0bd89b0609d1bf
Robin Shen <robin@onedev.io>
2024-01-10 20:38:54 +0800

    fix: Login with GitHub does not work when a user does not publish his email (#1712)

--- server-plugin/server-plugin-sso-openid/src/main/java/io/onedev/server/plugin/sso/openid/GitHubConnector.java
+++ server-plugin/server-plugin-sso-openid/src/main/java/io/onedev/server/plugin/sso/openid/GitHubConnector.java
@@ -1,16 +1,5 @@
 package io.onedev.server.plugin.sso.openid;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import org.apache.commons.lang3.StringUtils;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.wicket.request.cycle.RequestCycle;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.request.resource.PackageResourceReference;
-import org.apache.wicket.request.resource.ResourceReference;
-
 import com.nimbusds.oauth2.sdk.ParseException;
 import com.nimbusds.oauth2.sdk.SerializeException;
 import com.nimbusds.oauth2.sdk.http.HTTPResponse;
@@ -18,10 +7,19 @@ import com.nimbusds.oauth2.sdk.token.BearerAccessToken;
 import com.nimbusds.openid.connect.sdk.OIDCTokenResponse;
 import com.nimbusds.openid.connect.sdk.UserInfoErrorResponse;
 import com.nimbusds.openid.connect.sdk.UserInfoRequest;
-
-import io.onedev.server.model.support.administration.sso.SsoAuthenticated;
 import io.onedev.server.annotation.Editable;
+import io.onedev.server.model.support.administration.sso.SsoAuthenticated;
 import net.minidev.json.JSONObject;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.PackageResourceReference;
+import org.apache.wicket.request.resource.ResourceReference;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Editable(name="GitHub", order=100, description="Refer to this <a href='https://docs.onedev.io/tutorials/security/sso-with-github' target='_blank'>tutorial</a> for an example setup")
 public class GitHubConnector extends OpenIdConnector {
@@ -53,13 +51,18 @@ public class GitHubConnector extends OpenIdConnector {
 	}
 
 	@Override
+	protected String getBaseScope() {
+		return super.getBaseScope() + " user:email";
+	}
+
+	@Override
 	public String getGroupsClaim() {
 		return super.getGroupsClaim();
 	}
 
 	@Override
 	protected SsoAuthenticated processTokenResponse(OIDCTokenResponse tokenResponse) {
-		BearerAccessToken accessToken = (BearerAccessToken) tokenResponse.getTokens().getBearerAccessToken();
+		BearerAccessToken accessToken = tokenResponse.getTokens().getBearerAccessToken();
 
 		try {
 			UserInfoRequest userInfoRequest = new UserInfoRequest(
@@ -67,13 +70,29 @@ public class GitHubConnector extends OpenIdConnector {
 			HTTPResponse httpResponse = userInfoRequest.toHTTPRequest().send();
 
 			if (httpResponse.getStatusCode() == HTTPResponse.SC_OK) {
-				JSONObject json = httpResponse.getContentAsJSONObject();
-				String userName = (String) json.get("login");
-				String email = (String) json.get("email");
-				if (StringUtils.isBlank(email))
-					throw new AuthenticationException("A public email is required");
-				String fullName = (String) json.get("name");
-				
+				var jsonObject = httpResponse.getContentAsJSONObject();
+				var userName = jsonObject.getAsString("login");
+				var fullName = jsonObject.getAsString("name");
+				var email = jsonObject.getAsString("email");
+				if (StringUtils.isBlank(email)) {
+					userInfoRequest = new UserInfoRequest(
+							new URI("https://api.github.com/user/emails"), accessToken);
+					httpResponse = userInfoRequest.toHTTPRequest().send();
+					if (httpResponse.getStatusCode() == HTTPResponse.SC_OK) {
+						for (var element: httpResponse.getContentAsJSONArray()) {
+							JSONObject emailObject = (JSONObject) element;
+							if (emailObject.getAsString("verified").equals("true") 
+									&& emailObject.getAsString("primary").equals("true")) {
+								email = emailObject.getAsString("email");
+								break;
+							}
+						}
+						if (StringUtils.isBlank(email))
+							throw new AuthenticationException("A verified primary email address is required");
+					} else {
+						throw buildException(UserInfoErrorResponse.parse(httpResponse).getErrorObject());
+					}
+				}
 				return new SsoAuthenticated(userName, email, fullName, null, null, this);
 			} else {
 				throw buildException(UserInfoErrorResponse.parse(httpResponse).getErrorObject());
