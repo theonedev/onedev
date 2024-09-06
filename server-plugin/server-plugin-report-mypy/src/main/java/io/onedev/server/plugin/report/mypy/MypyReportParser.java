c3a4a9adc65354ba6275999da3acd8fcfb4a3176
Robin Shen <robin@onedev.io>
2024-09-06 12:31:04 +0800

    feat: Python mypy report parse and publish (OD-2061)

---
+++ server-plugin/server-plugin-report-mypy/src/main/java/io/onedev/server/plugin/report/mypy/MypyReportParser.java
@@ -0,0 +1,117 @@
+package io.onedev.server.plugin.report.mypy;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import io.onedev.commons.utils.ExplicitException;
+import io.onedev.commons.utils.PlanarRange;
+import io.onedev.commons.utils.TaskLogger;
+import io.onedev.server.codequality.CodeProblem;
+import io.onedev.server.codequality.RepoTarget;
+import io.onedev.server.model.Build;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import javax.annotation.Nullable;
+import java.util.*;
+
+import static io.onedev.server.codequality.CodeProblem.Severity.LOW;
+import static io.onedev.server.codequality.CodeProblem.Severity.MEDIUM;
+import static java.lang.Integer.parseInt;
+
+public class MypyReportParser {
+	
+	private static ParsedLine parseLine(String line) {
+		ParsedLine parsedLine = new ParsedLine();
+		var fields = Splitter.on(':').splitToList(line); 
+		List<Integer> locations = new ArrayList<>();
+		for (int i=0; i<fields.size(); i++) {
+			var field = fields.get(i);
+			if (i == 0) {
+				parsedLine.filePath = field;
+			} else if (NumberUtils.isDigits(field)) {
+				locations.add(parseInt(field));
+			} else if (locations.isEmpty()) {
+				parsedLine.filePath += ":" + field;
+			} else {
+				if (locations.size() == 4) {
+					parsedLine.fromRow = locations.get(0);
+					parsedLine.fromColumn = locations.get(1);
+					parsedLine.toRow = locations.get(2);
+					parsedLine.toColumn = locations.get(3);
+				} else if (locations.size() == 2) {
+					parsedLine.fromRow = locations.get(0);
+					parsedLine.fromColumn = locations.get(1);
+					parsedLine.toRow = parsedLine.fromRow;
+					parsedLine.toColumn = -1;
+				} else {
+					parsedLine.fromRow = locations.get(0);
+					parsedLine.fromColumn = 1;
+					parsedLine.toRow = parsedLine.fromRow;
+					parsedLine.toColumn = -1;
+				}
+				parsedLine.error = field.trim().equals("error");
+				var message = Joiner.on(':').join(fields.subList(i+1, fields.size()));
+				if (message.startsWith("  ")) {
+					parsedLine.complementary = true;
+					parsedLine.message = message.substring(2);
+				} else {
+					parsedLine.message = message.substring(1);
+				}
+				return parsedLine;
+			}
+		}
+		throw new ExplicitException("Error parsing mypy output: no message found");
+	}
+	
+	@Nullable
+	private static void populateCodeProblems(List<CodeProblem> problems, Build build, Map<String, Optional<String>> blobPaths, 
+									   ParsedLine parsedLine, TaskLogger logger) {
+		var blobPath = blobPaths.get(parsedLine.filePath);
+		if (blobPath == null) {
+			blobPath = Optional.ofNullable(build.getBlobPath(parsedLine.filePath));
+			if (blobPath.isEmpty())
+				logger.warning("Unable to find blob path for file: " + parsedLine.filePath);
+			blobPaths.put(parsedLine.filePath, blobPath);
+		}
+		if (blobPath.isPresent()) {
+			var location = new PlanarRange(parsedLine.fromRow-1, parsedLine.fromColumn-1, parsedLine.toRow-1, parsedLine.toColumn, 1);
+			var severity = parsedLine.error?MEDIUM:LOW;
+			problems.add(new CodeProblem(severity, new RepoTarget(blobPath.get(), location), parsedLine.message));
+		}
+	}
+	
+	public static List<CodeProblem> parse(Build build, List<String> output, TaskLogger logger) {
+		List<CodeProblem> problems = new ArrayList<>();
+		Map<String, Optional<String>> blobPaths = new HashMap<>();
+		ParsedLine leadingParsedLine = null;
+		for (var line: output) {
+			if (line.contains(":")) {
+				var parsedLine = parseLine(line);
+				if (leadingParsedLine == null) {
+					leadingParsedLine = parsedLine;
+				} else if (parsedLine.complementary) {
+					leadingParsedLine.message += "\n" + parsedLine.message;
+				} else {
+					populateCodeProblems(problems, build, blobPaths, leadingParsedLine, logger);
+					leadingParsedLine = parsedLine;
+				}
+			}
+		}
+		if (leadingParsedLine != null)
+			populateCodeProblems(problems, build, blobPaths, leadingParsedLine, logger);			
+		return problems;
+	}
+
+	private static class ParsedLine {
+
+		String filePath;
+		
+		int fromRow, fromColumn, toRow, toColumn;
+		
+		boolean error;
+		
+		boolean complementary;
+		
+		String message;
+		
+	}
+}
---
+++ server-plugin/server-plugin-report-mypy/src/test/java/io/onedev/server/plugin/report/mypy/MypyReportParserTest.java
@@ -0,0 +1,81 @@
+package io.onedev.server.plugin.report.mypy;
+
+import com.google.common.io.Resources;
+import io.onedev.commons.utils.TaskLogger;
+import io.onedev.server.codequality.CodeProblem;
+import io.onedev.server.codequality.RepoTarget;
+import io.onedev.server.model.Build;
+import org.apache.commons.io.IOUtils;
+import org.eclipse.jgit.lib.ObjectId;
+import org.jetbrains.annotations.Nullable;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
+public class MypyReportParserTest {
+
+	@Test
+	public void testParse() {
+		try (InputStream is = Resources.getResource(MypyReportParserTest.class, "mypy-output").openStream()) {
+			Build build = new Build() {
+				@Override
+				public String getBlobPath(String filePath) {
+					return filePath;
+				}
+			};
+			build.setCommitHash(ObjectId.zeroId().name());
+			var output = IOUtils.readLines(is, StandardCharsets.UTF_8);
+			var problems = MypyReportParser.parse(build, output, new TaskLogger() {
+				@Override
+				public void log(String message, @Nullable String sessionId) {
+				}
+			});
+			
+			assertEquals(10, problems.size());
+			
+			var problem = problems.get(0);
+			assertEquals(CodeProblem.Severity.MEDIUM, problem.getSeverity());
+			assertEquals("6.1-6.2-1", ((RepoTarget)problem.getTarget()).getLocation().toString());
+			assertEquals("torchtune/utils/_version.py", problem.getTarget().getGroupKey().getName());
+			assertEquals("Cannot find implementation or library stub for module named \"torch\"  [import-not-found]", problem.getMessage());
+
+			problem = problems.get(1);
+			assertEquals(CodeProblem.Severity.MEDIUM, problem.getSeverity());
+			assertEquals("10.1-10.2-1", ((RepoTarget)problem.getTarget()).getLocation().toString());
+			assertEquals("torchtune/utils:/_device.py", problem.getTarget().getGroupKey().getName());
+			assertEquals("Cannot find implementation or library stub for module named \"torch\"  [import-not-found]", problem.getMessage());
+
+			problem = problems.get(2);
+			assertEquals(CodeProblem.Severity.LOW, problem.getSeverity());
+			assertEquals("PEP 484 prohibits implicit Optional. Accordingly, mypy has changed its default to no_implicit_optional=True", problem.getMessage());
+
+			problem = problems.get(4);
+			assertEquals(CodeProblem.Severity.MEDIUM, problem.getSeverity());
+			assertEquals("88.5-135.22-1", ((RepoTarget)problem.getTarget()).getLocation().toString());
+			assertEquals("" +
+					"Signature of \"encode\" incompatible with supertype \"BaseTokenizer\"  [override]\n" +
+					"    Superclass:\n" +
+					"        def encode(self, text: str, **kwargs: dict[str, Any]) -> list[int]\n" +
+					"    Subclass:\n" +
+					"        def encode(self, text: str, add_bos: bool = ..., add_eos: bool = ...) -> list[int]", 
+					problem.getMessage());
+			
+			assertEquals(CodeProblem.Severity.MEDIUM, problems.get(5).getSeverity());
+			assertEquals(CodeProblem.Severity.LOW, problems.get(6).getSeverity());
+			
+			problem = problems.get(8);
+			assertEquals("174.21-174.0-1", ((RepoTarget)problem.getTarget()).getLocation().toString());
+			
+			problem = problems.get(9);
+			assertEquals(CodeProblem.Severity.MEDIUM, problem.getSeverity());
+			assertEquals("109.1-109.0-1", ((RepoTarget)problem.getTarget()).getLocation().toString());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
