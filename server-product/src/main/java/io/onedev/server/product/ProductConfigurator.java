43096b8615d874ec1a878d2c895b9056ec7cce69
Robin Shen <robin@onedev.io>
2026-01-25 15:00:04 +0800

    fix: Also listen to ipv6 loopback when bind to specific ip address for http service

--- server-product/src/main/java/io/onedev/server/product/ProductConfigurator.java
+++ server-product/src/main/java/io/onedev/server/product/ProductConfigurator.java
@@ -6,6 +6,8 @@ import org.eclipse.jetty.server.HttpConfiguration;
 import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.onedev.server.ServerConfig;
 import io.onedev.server.jetty.ServerConfigurator;
@@ -13,6 +15,8 @@ import io.onedev.server.jetty.ServerConfigurator;
 public class ProductConfigurator implements ServerConfigurator {
 
 	private static final int REQUEST_HEADER_SIZE = 16*1024;
+	
+	private static final Logger logger = LoggerFactory.getLogger(ProductConfigurator.class);
 	
 	private ServerConfig serverConfig;
 	
@@ -33,16 +37,31 @@ public class ProductConfigurator implements ServerConfigurator {
 		connector.addConnectionFactory(new HttpConnectionFactory(configuration));
 		server.addConnector(connector);
 		
-		// If http_host is a specific IP (not 0.0.0.0), also bind to localhost as curl needs to access localhost
-		// for git operations
+		// If http_host is not a wildcard (0.0.0.0 or ::), also bind to both IPv4 and IPv6 
+		// loopback addresses as curl needs to access localhost for git operations
 		String httpHost = serverConfig.getHttpHost();
-		if (!"0.0.0.0".equals(httpHost) && !"127.0.0.1".equals(httpHost) && !"localhost".equals(httpHost)) {
-			ServerConnector localhostConnector = new ServerConnector(server);
-			localhostConnector.setHost("127.0.0.1");
-			localhostConnector.setPort(serverConfig.getHttpPort());
-			localhostConnector.addConnectionFactory(new HttpConnectionFactory(configuration));
-			server.addConnector(localhostConnector);
+		
+		if (!"0.0.0.0".equals(httpHost) && !"::".equals(httpHost)) {
+			if (!"127.0.0.1".equals(httpHost)) {
+				var localhostConnector = new ServerConnector(server);
+				localhostConnector.setHost("127.0.0.1");
+				localhostConnector.setPort(serverConfig.getHttpPort());
+				localhostConnector.addConnectionFactory(new HttpConnectionFactory(configuration));
+				server.addConnector(localhostConnector);					
+			}			
+			if (!"::1".equals(httpHost)) {
+				try {
+					var localhostConnector = new ServerConnector(server);
+					localhostConnector.setHost("::1");
+					localhostConnector.setPort(serverConfig.getHttpPort());
+					localhostConnector.addConnectionFactory(new HttpConnectionFactory(configuration));
+					server.addConnector(localhostConnector);
+				} catch (Exception e) {
+					logger.debug("Unable to bind to IPv6 loopback address ::1, IPv6 may not be available", e);
+				}
+			}
 		}
+
 	}
 
 }
