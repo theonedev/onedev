49d99fa07298c12be306f6961aca87ae600d4655
Robin Shen <robin@onedev.io>
2026-06-03 07:20:18 +0800

    chore: Bump version

--- server-core/src/main/java/io/onedev/server/workspace/WorkspaceProvisionerDiscoverer.java
+++ server-core/src/main/java/io/onedev/server/workspace/WorkspaceProvisionerDiscoverer.java
@@ -1,9 +1,12 @@
 package io.onedev.server.workspace;
 
+import org.jspecify.annotations.Nullable;
+
 import io.onedev.server.model.support.administration.workspaceprovisioner.WorkspaceProvisioner;
 
 public interface WorkspaceProvisionerDiscoverer {
 	
+    @Nullable
     WorkspaceProvisioner discover();
 
 	int getOrder();
